ffea7488d59330e6166b4f555232b0427587a49f
rmsmith95 <rmsmith95@hotmail.com>
2026-02-21 09:42:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # Allow React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    allow_origins=["http://localhost:3000", "http://localhost:3001", "http://192.168.1.249"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
